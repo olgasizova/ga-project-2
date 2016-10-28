31eaec1cea8d616905d74775a40fe1809b265085
Olga Sizova <olisizova@gmail.com>
2016-10-27 21:44:07 -0400

    MVC architecture is set up and server is listening on 3000

---
+++ app.js
@@ -0,0 +1,21 @@
+/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
+/* eslint key-spacing: ["error", { align: "value" }] */
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
+const express       = require('express');
+const logger        = require('morgan');
+
+const loginRoute    = require('./routes/login');
+
+const app           = express();
+const PORT          = process.argv[2] || process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.listen(PORT, () => console.warn('Onboard server is listening on ', PORT));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+app.use('/', loginRoute);
---
+++ routes/login.js
@@ -0,0 +1,6 @@
+const Router = require('express').Router();
+
+
+
+
+module.exports = Router;
